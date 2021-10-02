ee2d02cd0e4190dbbb30a8057ecfccfc684c333c
Dzmitry.Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-10-02 12:46:12 +0300

    Add react-native-elements lib to project, add basic test theme for button

--- LabProject/App.tsx
+++ LabProject/App.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/pages/Home';
+import { ThemeProvider } from 'react-native-elements';
+import theme from './src/theme/theme';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={Home} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <ThemeProvider theme={theme}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={Home} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </ThemeProvider>
     );
 };
 
---
+++ LabProject/src/theme/theme.ts
@@ -0,0 +1,13 @@
+const theme = {
+    Button: {
+        raised: true,
+        titleStyle: {
+            color: 'red',
+        },
+        buttonStyle: {
+            backgroundColor: 'pink',
+        },
+    },
+};
+
+export default theme;
