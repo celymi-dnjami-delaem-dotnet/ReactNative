29740a57c55a3a83834b3a066319294f1aa7864a
Dzmitry.Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-10-02 14:24:49 +0300

    Rename pages to screens folder

--- LabProject/App.tsx
+++ LabProject/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/pages/Home';
+import Home from './src/screens/Home';
 import { ThemeProvider } from 'react-native-elements';
 import theme from './src/theme/theme';
 
