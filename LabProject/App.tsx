ff1761edcd8da5004b4535023798b74338ca64fc
Dzmitry.Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2021-10-18 19:59:59 +0300

    Add splash-screen library for launching application, update splash screen for IOS app

--- LabProject/App.tsx
+++ LabProject/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from 'react-native-elements';
 import theme from './src/theme/theme';
@@ -7,21 +7,28 @@ import { Provider } from 'react-redux';
 import Navigator from './src/screens/Navigator';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { PersistGate } from 'redux-persist/integration/react';
+import SplashScreen from 'react-native-splash-screen';
 
 Icon.loadFont();
 
 const { store, persistor } = configureStore();
 
-const App = () => (
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <ThemeProvider theme={theme}>
-                <NavigationContainer>
-                    <Navigator />
-                </NavigationContainer>
-            </ThemeProvider>
-        </PersistGate>
-    </Provider>
-);
+const App = () => {
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
+
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <ThemeProvider theme={theme}>
+                    <NavigationContainer>
+                        <Navigator />
+                    </NavigationContainer>
+                </ThemeProvider>
+            </PersistGate>
+        </Provider>
+    );
+};
 
 export default App;
